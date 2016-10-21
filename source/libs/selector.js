4bc846ec32b2d4f8f9f20ea2491bd360d50e08f7
Pat <patt.gerr@gmail.com>
2016-10-21 12:08:46 +0200

    added selector.js

---
+++ source/libs/selector.js
@@ -0,0 +1,120 @@
+/*
+    JQuery-esque selector for DOM elements
+
+    usage:
+
+        let elements = selector('.my-selector');
+
+        elements.css({
+            'background-color': 'red',
+            fontSize: '30px'
+        });
+
+        elements.each((element) => {
+            // do stuff with element
+        });
+
+============================================================================ */
+
+function selector(selectors, parent = document) {
+    let nodes;
+    const chop = Array.prototype.slice;
+
+
+    /*
+        Node
+    ------------------------------------ */
+
+    function getNodes() {
+        nodes = chop.call(parent.querySelectorAll(selectors));
+    }
+
+    function eachNode(callback) {
+        nodes.map(node => callback(node));
+
+        return this;
+    }
+
+    /*
+        Css
+    ------------------------------------ */
+
+    function changeNodeCss(css) {
+        for (const key of Object.keys(css)) {
+            eachNode(node => {
+                node.style[key] = css[key]; // eslint-disable-line
+            });
+        }
+
+        return this;
+    }
+
+    /*
+        Classes
+    ------------------------------------ */
+
+    function addClass(classes) {
+        eachNode(element => element.classList.add(classes));
+
+        return this;
+    }
+
+    function removeClass(classes) {
+        eachNode(element => element.classList.remove(classes));
+
+        return this;
+    }
+
+    function toggleClass(classes) {
+        eachNode(element => element.classList.toggle(classes));
+
+        return this;
+    }
+
+
+    /*
+        Events
+    ------------------------------------ */
+
+    function addEvents(eventNames, callback) {
+        eachNode(element => {
+            eventNames.split(' ').map(eventName => element.addEventListener(eventName, callback));
+        });
+
+        return this;
+    }
+
+    function removeEvents(eventNames, callback) {
+        eachNode(element => {
+            eventNames.split(' ').map(eventName => {
+                element.removeEventListener(eventName, callback);
+            });
+        });
+
+        return this;
+    }
+
+
+    /*
+        initialize
+    ------------------------------------ */
+
+    function init() {
+        getNodes();
+    }
+
+    init();
+
+    return {
+        nodes,
+        addClass,
+        removeClass,
+        toggleClass,
+        on: addEvents,
+        off: removeEvents,
+        css: changeNodeCss,
+        each: eachNode,
+    };
+}
+
+export default selector;
