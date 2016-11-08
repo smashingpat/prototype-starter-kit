be90ae64a0f32944ceab8806e197937d99756e03
smashingpat <patt.gerr@gmail.com>
2016-11-08 13:20:07 +0100

    replaced budo with Watchify and Browser-sync

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # prototype-starter-kit
 
-A quick boilerplate for prototyping javascript using [budõ][budo], [Gulp][gulp] and Sass + Postcss.
+A quick boilerplate for prototyping javascript using [Browserify][browserify], [Gulp][gulp], [Browser-sync][bs] and Sass + Postcss.
 
 ## Usage
 
@@ -18,7 +18,7 @@ npm start
 npm run bundle
 ```
 
-This starts a watch server and open `localhost:9966` in your default browser. Changes made to `source/index.js` will bundle the files and refreshes the browser. Changes made to `source/sass/*.scss` will cause CSS injection without losing the current state.
+This starts a watch server and open `localhost:3000` in your default browser. Changes made to `source/index.js` will bundle the files and refreshes the browser. Changes made to `source/sass/*.scss` will cause CSS injection without losing the current state.
 
 ## Tasks
 ```sh
@@ -28,5 +28,6 @@ npm run bundle  # build compressed files
 npm run deploy  # build compressed files and copies contents app folder to subtree gh-pages
 ```
 
-[budo]: https://github.com/mattdesl/budo
+[browserify]: http://browserify.org/
+[bs]: https://www.browsersync.io/
 [gulp]: https://github.com/gulpjs/gulp
--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,29 @@
 
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
+const glob = require('glob')
+const path = require('path')
+const es = require('event-stream')
 const gutil = require('gulp-util')
 const plumber = require('gulp-plumber')
 const sourcemaps = require('gulp-sourcemaps')
 const watch = require('gulp-watch')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
-const budo = require('budo')
 const argv = require('yargs').argv;
+
 const uglify = require('gulp-uglify')
 const rename = require('gulp-rename')
 const stream = require('gulp-streamify')
 const source = require('vinyl-source-stream')
 const browserify = require('browserify')
+const watchify = require('watchify')
 const babelify  = require('babelify')
 
-
+const browserSync = require('browser-sync')
 
 const entry = './source/index.js'
 const outfile = 'bundle.js'
-
-const browserifyConfig = {
-    transform: babelify
-}
 
 const tasks = {
     sass: function sassTask() {
@@ -51,43 +51,80 @@ const tasks = {
                 ]))
             .pipe(gulpif(!argv.production, sourcemaps.write()))
             .pipe(gulp.dest('./app'))
+            .pipe(browserSync.stream())
 
     },
-    script: function scriptTask() {
+    script: function scriptTask(userSettings, callback) {
 
-        var bundler = browserify(entry, browserifyConfig).bundle()
+        const defaultSettings = {
+            watch: false,
+            entries: './source/*.js'
+        }
 
-        return bundler
-            .pipe(source('index.js'))
-            .pipe(gulpif(argv.production, stream(uglify({
-                output: {
-                    beautify: argv.beautify ? true : false
+        const settings = Object.assign({}, defaultSettings, userSettings);
+
+        glob(settings.entries, (error, files) => {
+
+            const streams = files.map(entry => {
+                const filename = path.relative('./source/', entry);
+
+                let b = browserify(entry, {
+                    transform: babelify,
+                })
+
+                b = settings.watch ? watchify(b) : b
+
+                const bundle = () => {
+                    return b.bundle()
+                        .on('error', function(err) {
+                            browserSync.notify(`[<span style="color:red">Browserify</span>] ERROR`);
+                            gutil.log(`[${gutil.colors.cyan('browserify')}] ${gutil.colors.red('Error:')}\n ${err} \n\n${err.codeFrame}`);
+                            this.emit('end');
+                        })
+                        .pipe(source(filename))
+                        .pipe(gulpif(argv.production, stream(uglify({
+                            output: {
+                                beautify: argv.beautify ? true : false
+                            }
+                        }))))
+                        .pipe(gulp.dest('./app'))
+                        .pipe(browserSync.stream())
                 }
-            }))))
-            .pipe(rename(outfile))
-            .pipe(gulp.dest('./app'))
+
+                b.on('update', bundle)
+                b.on('log', gutil.log)
+
+                return bundle()
+            })
+
+            es.merge(streams).on('end', callback)
+        });
 
     },
     server: function createServer(callback) {
 
         // dev server
-        let server = budo(entry, {
-            serve: outfile,
-            port: argv.port ? argv.port : 3000,
-            live: true,
-            dir: './app',
+        let server = browserSync({
+            server: {
+                baseDir: "./app/"
+            },
             open: argv.open,
-            browserify: browserifyConfig,
-            stream: process.stdout
-        }).on('exit', callback)
+            port: argv.port ? argv.port : 3000,
+        }, callback)
 
-        // watch files
-        watch(['source/**/*.{scss,sass}'], tasks.sass)
     },
+    watch: function watchFiels(callback) {
+        watch(['app/**/*.{html,json}'], browserSync.reload)
+        watch(['source/**/*.{scss,sass}'], tasks.sass)
+        tasks.script({watch:true}, callback)
+    }
 }
 
 gulp.task('sass', tasks.sass)
-gulp.task('script', tasks.script)
+gulp.task('script', cb => tasks.script({}, cb))
 
-gulp.task('watch', ['sass'], tasks.server)
+gulp.task('watch', tasks.watch)
+gulp.task('server', tasks.server)
+
+gulp.task('serve', ['sass', 'watch', 'server'])
 gulp.task('bundle', ['sass', 'script'])
