272a595829c98d3255c29c2ee2b1d8c34c83640b
smashingpat <patt.gerr@gmail.com>
2016-11-28 09:19:23 +0100

    converted indents from 4 to 2

--- source/utils/breakpoint.js
+++ source/utils/breakpoint.js
@@ -1,50 +1,50 @@
 const defaultParams = {
-    element: document.body
+  element: document.body
 }
 
 const Breakpoint = function(params = defaultParams) {
-    this.element = params.element
-    this.afterElement = window.getComputedStyle(this.element, ':after')
-    this.currentBreakpoint = ''
-    this.lastBreakpoint = ''
-    this.functions = {};
-    this.init()
+  this.element = params.element
+  this.afterElement = window.getComputedStyle(this.element, ':after')
+  this.currentBreakpoint = ''
+  this.lastBreakpoint = ''
+  this.functions = {};
+  this.init()
 }
 
 Breakpoint.prototype = {
-    init() {
-        window.addEventListener('resize', this._resizeHandler.bind(this), false)
-    },
-    on(breakPointName, callback) {
-        let functionArray = this.functions[breakPointName] || [];
+  init() {
+    window.addEventListener('resize', this._resizeHandler.bind(this), false)
+  },
+  on(breakPointName, callback) {
+    let functionArray = this.functions[breakPointName] || [];
 
-        this.functions[breakPointName] = functionArray.concat(callback)
+    this.functions[breakPointName] = functionArray.concat(callback)
 
-        return this
-    },
-    call() {
-        this._resizeHandler()
-        return this
-    },
-    _resizeHandler() {
-        // check current breakpoint
-        this.currentBreakpoint = this.afterElement
-            .getPropertyValue('content')
-            .replace(/^['"]|['"]$/g, '')
+    return this
+  },
+  call() {
+    this._resizeHandler()
+    return this
+  },
+  _resizeHandler() {
+    // check current breakpoint
+    this.currentBreakpoint = this.afterElement
+      .getPropertyValue('content')
+      .replace(/^['"]|['"]$/g, '')
 
-        if (this.currentBreakpoint !== this.lastBreakpoint) {
-            this.lastBreakpoint = this.currentBreakpoint // set last breakpoint to current
+    if (this.currentBreakpoint !== this.lastBreakpoint) {
+      this.lastBreakpoint = this.currentBreakpoint // set last breakpoint to current
 
-            this._callFunctions()
-        }
-    },
-    _callFunctions() {
-        let functionsList = this.functions[this.currentBreakpoint] || []
-
-        if (functionsList.length > 0) {
-            functionsList.map(fn => fn())
-        }
+      this._callFunctions()
     }
+  },
+  _callFunctions() {
+    let functionsList = this.functions[this.currentBreakpoint] || []
+
+    if (functionsList.length > 0) {
+      functionsList.map(fn => fn())
+    }
+  }
 }
 
 
