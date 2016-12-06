9489723cfbe04acfca746f9062960f3499c950dc
smashingpat <patt.gerr@gmail.com>
2016-12-06 13:11:22 +0100

    made react demo

--- source/bundle.js
+++ source/bundle.js
@@ -1,118 +1,127 @@
-import times from './utils/times'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import imageSlicer from './image-slicer'
 
-/*
-    Create the image and wait for it to be loaded
------------------------------------- */
-const imagePath = '/image.jpg'
-const image = new Image()
-
-image.onload = function() {
-    const data = getImageData(this);
-
-    data.dataurl.map(node => {
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            imageUrl: '/image.jpg',
+            width: 0,
+            height: 0,
+            dataurl: [],
+            showOutlines: false,
+            showOriginal: false,
+            showDifference: false,
+            maxSize: 500
+        }
+        this.renderSlicedImage()
+    }
+    renderSlicedImage() {
         const image = new Image()
-        image.src = node.imageData
 
-        document.getElementById('imageContainer').appendChild(image)
-    })
-}
-
-// load image
-image.src = imagePath
-
-
-/*
-    Set input field that changes the above image
------------------------------------- */
-const input = document.getElementById('upload');
-
-input.addEventListener('change', function(event) {
-     image.src = URL.createObjectURL(event.target.files[0]);
-})
-
-
-/*
-    Create the canvas and split it
------------------------------------- */
-function getImageData(image, userSettings) {
-    const initialSettings = {
-        horizontal: 17,
-        vertical: 12,
-        maxSize: 800,
-    }
-    const settings = Object.assign({}, initialSettings, userSettings);
-    const slices = (settings.horizontal * settings.vertical)
-    const sizes = calculateSizes()
-    const imageHeight = sizes.height
-    const imageWidth = sizes.width
-
-    let dataurl;
-
-    function calculateSizes() {
-        const { maxSize } = settings
-        const { width, height } = image;
-        let newSize = {
-            height,
-            width,
-        }
-
-        if (width > maxSize && height > maxSize) {
-            if (width > height) {
-                newSize.width = maxSize
-                newSize.height = maxSize * (height / width)
-            } else {
-                newSize.width = maxSize * (width / height)
-                newSize.height = maxSize
-            }
-        }
-
-        return newSize
-    }
-
-    function splitImageData() {
-        const { horizontal, vertical } = settings
-        const imageWidthSlice = imageWidth / horizontal
-        const imageHeightSlice = imageHeight / vertical
-
-        let data = []
-
-        let x = 0
-        let y = 0
-
-        times(slices)((index) => {
-            const canvas = document.createElement('canvas')
-            const context = canvas.getContext('2d')
-
-            canvas.height = imageHeightSlice
-            canvas.width = imageWidthSlice
-
-            // Draw the image
-            context.drawImage(image, x, y, imageWidth, imageHeight)
-
-            // Move the pointers
-            if ((index + 1) % horizontal != 0) {
-                x = x - imageWidthSlice
-            } else {
-                x = 0
-                y = y - imageHeightSlice
-            }
-
-            data.push({
-                index,
-                imageData: canvas.toDataURL()
+        image.onload = () => {
+            const data = imageSlicer(image, {
+                maxSize: this.state.maxSize
+            });
+            const { dataurl } = data;
+            const { height, width } = data.totalSize;
+            this.setState({
+                width,
+                height,
+                dataurl,
             })
-        })
+        }
 
-        return data
+        image.src = this.state.imageUrl
     }
-
-    dataurl = splitImageData()
-
-    return {
-        totalSize: {
-            height: imageHeight,
-            width: imageWidth,
-        },
-        dataurl
+    toggleOutline() {
+        this.setState({
+            showOutlines: !this.state.showOutlines
+        })
+    }
+    toggleOriginal() {
+        this.setState({
+            showOriginal: !this.state.showOriginal
+        })
+    }
+    toggleDifference() {
+        this.setState({
+            showOriginal: true,
+            showDifference: !this.state.showDifference
+        })
+    }
+    render() {
+        return (
+            <div className="Wrapper">
+                <div>
+                    <button onClick={this.toggleOutline.bind(this)}>toggle outline</button>
+                    <button onClick={this.toggleOriginal.bind(this)}>toggle original</button>
+                    <button onClick={this.toggleDifference.bind(this)}>toggle difference</button>
+                </div>
+                <div style={{
+                    position: 'relative',
+                }}>
+                    <div style={{
+                        display: 'inline-block',
+                        width: this.state.width,
+                        fontSize: 0,
+                    }}>
+                        {this.state.dataurl.map(data => {
+                            let styles = {
+                                display: 'inline-block',
+                                outline: this.state.showOutlines && '2px solid blue',
+                            }
+                            return (
+                                <img style={styles} key={data.index} src={data.imageData} />
+                            )
+                        })}
+                    </div>
+                    <img src={this.state.imageUrl} width={this.state.width} style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        filter: !this.state.showDifference && 'invert(100%) opacity(50%)',
+                        display: !this.state.showOriginal && 'none',
+                        outline: '5px solid green',
+                    }}/>
+                </div>
+            </div>
+        )
     }
 }
+
+render(
+    <App/>,
+    document.getElementById('root')
+)
+
+/*
+//     Create the image and wait for it to be loaded
+// ------------------------------------ */
+// const imagePath = '/image.jpg'
+// const image = new Image()
+//
+// image.onload = function() {
+//     const data = imageSlicer(this);
+//
+//     data.dataurl.map(node => {
+//         const image = new Image()
+//         image.src = node.imageData
+//
+//         document.getElementById('imageContainer').appendChild(image)
+//     })
+// }
+//
+// // load image
+// image.src = imagePath
+//
+//
+// /*
+//     Set input field that changes the above image
+// ------------------------------------ */
+// const input = document.getElementById('upload');
+//
+// input.addEventListener('change', function(event) {
+//      image.src = URL.createObjectURL(event.target.files[0]);
+// })
---
+++ source/image-slicer/index.js
@@ -0,0 +1,87 @@
+import times from '../utils/times'
+
+function imageSlicer(image, userSettings) {
+    const initialSettings = {
+        horizontal: 2,
+        vertical: 2,
+        maxSize: 1000,
+    }
+    const settings = Object.assign({}, initialSettings, userSettings);
+    console.log(settings);
+    const slices = (settings.horizontal * settings.vertical)
+    const sizes = calculateSizes()
+    const imageHeight = sizes.height
+    const imageWidth = sizes.width
+
+    let dataurl;
+
+    function calculateSizes() {
+        const { maxSize } = settings
+        const { width, height } = image;
+        let newSize = {
+            height,
+            width,
+        }
+
+        if ( maxSize && width > maxSize && height > maxSize) {
+            if (width > height) {
+                newSize.width = maxSize
+                newSize.height = maxSize * (height / width)
+            } else {
+                newSize.width = maxSize * (width / height)
+                newSize.height = maxSize
+            }
+        }
+
+        return newSize
+    }
+
+    function splitImageData() {
+        const { horizontal, vertical } = settings
+        const imageWidthSlice = imageWidth / horizontal
+        const imageHeightSlice = imageHeight / vertical
+
+        let data = []
+
+        let x = 0
+        let y = 0
+
+        times(slices)((index) => {
+            const canvas = document.createElement('canvas')
+            const context = canvas.getContext('2d')
+
+            canvas.height = imageHeightSlice
+            canvas.width = imageWidthSlice
+
+            // Draw the image
+            context.drawImage(image, x, y, imageWidth, imageHeight)
+
+            // Move the pointers
+            if ((index + 1) % horizontal != 0) {
+                x = x - imageWidthSlice
+            } else {
+                x = 0
+                y = y - imageHeightSlice
+            }
+
+            data.push({
+                index,
+                imageData: canvas.toDataURL()
+            })
+        })
+
+        return data
+    }
+
+    dataurl = splitImageData()
+
+    return {
+        totalSize: {
+            height: imageHeight,
+            width: imageWidth,
+        },
+        dataurl
+    }
+}
+
+export default imageSlicer
