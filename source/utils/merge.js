75bfdd411dfd9faaa7de7b9a29663b97f03da230
smashingpat <patt.gerr@gmail.com>
2016-12-17 20:13:59 +0100

    added zuul and tape for testing
    s
    
    s

---
+++ source/utils/merge.js
@@ -0,0 +1,11 @@
+/*
+    Merge
+    -----
+
+    Merges all objects together
+    in order.
+------------------------------------------ */
+
+export default function(...objects) {
+    return Object.assign({}, ...objects);
+}
