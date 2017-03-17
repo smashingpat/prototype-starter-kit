f60ae926010f2b3642f85ac463419e5b9906253e
smashingpat <patt.gerr@gmail.com>
2017-03-17 16:33:59 +0100

    added plane

--- source/bundle.js
+++ source/bundle.js
@@ -1,57 +1,115 @@
+import range from './utils/range';
+
+
 const defaultSettings = {
     width: 700,
     height: 500,
 };
 
-const spaceinvader = (canvasElement, userSettings = {}) => {
+const spaceInvader = (canvasElement, userSettings = {}) => {
     const canvas = canvasElement;
     const ctx = canvas.getContext('2d');
     const settings = { ...defaultSettings, ...userSettings };
-    const invaderWidth = 30;
-    const invaderHeight = 20;
-    const missileWidth = 2;
-    const missileHeight = 5;
+    const CANVAS_WIDTH = settings.width;
+    const CANVAS_HEIGHT = settings.height;
+    const PLAYER_WIDTH = 30;
+    const PLAYER_HEIGHT = 20;
+    const INVADER_WIDTH = 40;
+    const INVADER_HEIGHT = 26;
+    const INVADER_PLANE_WIDTH = INVADER_WIDTH * 8;
+    const INVADER_PLANE_HEIGHT = INVADER_HEIGHT * 5;
+    const MISSILE_WIDTH = 4;
+    const MISSILE_HEIGHT = 8;
 
     let mouseX = 0;
     let mouseY = 0;
-    let invaderX = 0;
-    let invaderY = 0;
-    const enemies = [];
+    let playerX = 0;
+    let playerY = 0;
+    let invaderPlane = null;
     const missiles = [];
 
-    const createEnemy = (initialX, initialY) => {
+    const createInvader = (initialX, initialY) => {
+        let didHit = false;
         let x = initialX;
         let y = initialY;
-        const size = 50;
+        return {
+            get didHit() { return didHit; },
+            draw() {
+                if (!didHit) {
+                    ctx.fillStyle = 'red';
+                    ctx.fillRect(
+                        x,
+                        y,
+                        INVADER_WIDTH,
+                        INVADER_HEIGHT,
+                    );
+                }
+                y += 1;
+            },
+            checkHit(hits) {
+                hits.forEach((hit) => {
+                    if (
+                        (!hit.didHit) &&
+                        (!didHit) &&
+                        (hit.x + MISSILE_WIDTH > x && hit.x < x + INVADER_WIDTH) &&
+                        (hit.y + MISSILE_HEIGHT > y && hit.y < y + INVADER_HEIGHT)
+                    ) {
+                        hit.hit();
+                        this.hit();
+                        this.destroy();
+                    }
+                });
+            },
+            hit() {
+                didHit = true;
+            },
+            destroy() {
+                setTimeout(() => {
+                    enemies.splice(enemies.indexOf(this), 1);
+                }, 0);
+            },
+        };
+    };
+
+    const createInvaderPlane = () => {
+        const invaders = [];
+        let x = (CANVAS_WIDTH - INVADER_PLANE_WIDTH) / 2;
+        let y = 10;
+        let goingLeft = true;
+
         return {
             draw() {
-                x = mouseX;
-                y = mouseY;
-                ctx.fillStyle = 'rgba(0,0,0,.4)';
+                ctx.fillStyle = 'rgba(255,50,50,.2)';
                 ctx.fillRect(
                     x,
                     y,
-                    size,
-                    size,
+                    INVADER_PLANE_WIDTH,
+                    INVADER_PLANE_HEIGHT,
                 );
-            },
-            checkHit(missiles) {
-                missiles.forEach(missile => {
-                    if (
-                        (missile.x > x && missile.x < x + size) &&
-                        (missile.y > y && missile.y < y + size)
-                    ) {
-                        missile.hit();
-                    }
-                })
+                if (goingLeft) {
+                    x += 18;
+                } else {
+                    x -= 18;
+                }
+                if (x + INVADER_PLANE_WIDTH > CANVAS_WIDTH) {
+                    goingLeft = false;
+                    y += INVADER_HEIGHT;
+                }
+                if (x < 0) {
+                    goingLeft = true;
+                    y += INVADER_HEIGHT;
+                }
+                if (y + INVADER_PLANE_HEIGHT > CANVAS_HEIGHT + INVADER_HEIGHT) {
+                    startGame();
+                }
             },
         };
     };
 
     const createMissile = (initialX, initialY) => {
-        const x = initialX + ((invaderWidth / 2) - (missileWidth / 2));
+        const x = initialX + ((PLAYER_WIDTH / 2) - (MISSILE_WIDTH / 2));
         let y = initialY;
-        let hit = false;
+        let didHit = false;
 
         return {
             get x() {
@@ -60,52 +118,54 @@ const spaceinvader = (canvasElement, userSettings = {}) => {
             get y() {
                 return y;
             },
+            get didHit() { return didHit; },
             draw() {
                 if (y < 0) {
                     this.remove();
                     return;
                 }
-                if (!hit) {
-                    ctx.fillStyle = 'cyan';
+                if (!didHit) {
+                    ctx.fillStyle = 'black';
                     ctx.fillRect(
                         x,
                         y,
-                        missileWidth,
-                        missileHeight,
+                        MISSILE_WIDTH,
+                        MISSILE_HEIGHT,
                     );
                 }
                 y -= 5;
             },
             hit() {
-                hit = true;
+                didHit = true;
             },
             remove() {
-                missiles.shift();
+                setTimeout(() => {
+                    missiles.splice(missiles.indexOf(this), 1);
+                }, 0);
             },
         };
     };
 
-    const invader = (x, y) => {
+    const player = (x, y) => {
         ctx.fillStyle = 'tomato';
-        invaderX = Math.max(Math.min(x - (invaderHeight / 2), canvas.width - invaderHeight), 0);
-        invaderY = Math.max(Math.min(y - (invaderWidth / 2), canvas.height - invaderWidth), 0);
+        playerX = Math.max(Math.min((x - (PLAYER_WIDTH / 2)), canvas.width - PLAYER_WIDTH), 0);
+        playerY = Math.max(Math.min((y - (PLAYER_HEIGHT / 2)), canvas.height - PLAYER_HEIGHT), 0); // eslint-disable-line max-len
 
         ctx.fillRect(
-            invaderX,
-            invaderY,
-            invaderWidth,
-            invaderHeight,
+            playerX,
+            playerY,
+            PLAYER_WIDTH,
+            PLAYER_HEIGHT,
         );
     };
 
     const mouseMoveHandler = (e) => {
         mouseX = e.clientX - canvas.offsetLeft;
         mouseY = e.clientY - canvas.offsetTop;
-        missiles.push(createMissile(invaderX, invaderY));
     };
 
     const mouseDownHandler = () => {
-        missiles.push(createMissile(invaderX, invaderY));
+        missiles.push(createMissile(playerX, playerY));
     };
 
     const addEventListeners = () => {
@@ -114,33 +174,42 @@ const spaceinvader = (canvasElement, userSettings = {}) => {
     };
 
     const render = () => {
-        ctx.fillStyle = 'black';
         ctx.clearRect(
             0,
             0,
-            settings.width,
-            settings.height,
+            CANVAS_WIDTH,
+            CANVAS_HEIGHT,
         );
 
         missiles.forEach(missile => missile.draw());
-        enemies.forEach((enemy) => {
-            enemy.draw();
-            enemy.checkHit(missiles);
-        });
-        invader(mouseX, canvas.height - 10);
+        player(mouseX, canvas.height - 60);
+
+        invaderPlane.draw();
 
         requestAnimationFrame(render, canvas);
     };
 
+    const gameOver = () => {
+
+    }
+
+    const startGame = () => {
+        missiles.splice(0, missiles.length);
+        invaderPlane = createInvaderPlane();
+    };
+
     const init = () => {
-        canvas.height = settings.height;
-        canvas.width = settings.width;
+        canvas.height = CANVAS_HEIGHT;
+        canvas.width = CANVAS_WIDTH;
         addEventListeners();
+        startGame();
         render();
-        enemies.push(createEnemy(100, 100));
+        // setInterval(() => {
+        //     enemies.push(createInvader(CANVAS_WIDTH / 2, 100));
+        // }, 1000);
     };
 
     init();
 };
 
-spaceinvader(document.getElementById('canvas'));
+spaceInvader(document.getElementById('canvas'));
